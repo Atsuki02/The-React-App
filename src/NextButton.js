df4a53c9b4df6f06ed9390d67d2fa87049d49cdc
Atsuki02 <canvanvam@gmail.com>
2023-06-17 17:39:45 -0700

    first commit

---
+++ src/NextButton.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function NextButton({ dispatch, answer, index, numQuesitons }) {
+  if (answer === null) return null;
+  if (index < numQuesitons - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  if (index === numQuesitons - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+}
+
+export default NextButton;
